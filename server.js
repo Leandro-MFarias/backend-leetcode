7511c64fa826b077c47013422119423ddb3bbef4
Leandro Farias <leandrorf1606@gmail.com>
2025-06-30 23:05:25 -0300

    feat: adjust server.js

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-import express from "express"
-import publicRoutes from "./routes/public.js"
-import privateRoutes from "./routes/private.js"
-import cors from "cors"
+import express from "express";
+import publicRoutes from "./routes/public.js";
+import privateRoutes from "./routes/private.js";
+import cors from "cors";
 
-import { auth } from "./middlewares/auth.js"
-import cookieParser from "cookie-parser"
+import { auth } from "./middlewares/auth.js";
+import cookieParser from "cookie-parser";
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://maisprati-leetcode.vercel.app",
+];
 
 const PORT = process.env.PORT || 3333
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.use(express.json())
-app.use(cors({ origin: "https://maisprati-leetcode.vercel.app", credentials: true}))
+app.use(
+  cors({ origin: allowedOrigins, credentials: true })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use("/", publicRoutes)
-app.use("/home", auth, privateRoutes)
+app.use("/", publicRoutes);
+app.use("/home", auth, privateRoutes);
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
-
-// https://maisprati-leetcode.vercel.app
-// http://localhost:3000
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
