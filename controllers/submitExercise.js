9a2c2e13ea9d2a241561c04ab858cbecb331586e
Leandro Farias <leandrorf1606@gmail.com>
2025-07-08 15:09:42 -0300

    feat: ensure testcase is working proprely

--- controllers/submitExercise.js
+++ controllers/submitExercise.js
@@ -7,21 +7,30 @@ export async function submitExercise(req, res) {
     return res.status(400).json({ message: "Algum campo está vazio" });
   }
 
-  const call = testCases
-    .map((tinput) => `console.log(${functionName}(${tinput.input}))`)
-    .join("\n");
+  const finalCase = `
+${sourceCode}
+function runTests() {
+  const results = [];
+  ${testCases
+    .map(
+      (t) =>
+        `results.push(${functionName}(${t.input}) === ${JSON.stringify(
+          t.expectedOutput
+        )});`
+    )
+    .join("\n")}
+  console.log(JSON.stringify(results));
+}
 
-  const finalCase = `${sourceCode}\n\n${call}`;
-  const expected = testCases.map((toutput) => toutput.expected).join("\n");
+runTests();
+`;
 
   try {
     const submission = await axios.post(
-      `${process.env.JUDGE0_API_URL}?wait=false`,
+      `${process.env.JUDGE0_API_URL}?wait=true`,
       {
         source_code: finalCase,
         language_id: languageId,
-        expected_output: expected,
-        stdin: "",
         redirect_stderr_to_stdout: true,
       },
       {
